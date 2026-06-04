19181dcea78438d2faeb980e54a64c1f611a9c29
Tonyg9000 <tonyg9000@gmail.com>
2026-06-04 16:28:08 -0700

    Delete gunicorn.conf.py

--- gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-# gunicorn.conf.py
-import os
-
-workers = 1          # Keep 1 worker so the scheduler only runs once
-threads = 4
-bind = f"0.0.0.0:{os.environ.get('PORT', '5000')}"
-timeout = 120
-loglevel = "info"
